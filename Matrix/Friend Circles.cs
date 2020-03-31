ec7af396fe12804ea77ed4df2db3de232d8a04ae
alifshits <alifshits@oneincsystems.com>
2020-03-31 11:02:16 +0700

    Add 1 task.

---
+++ Matrix/Friend Circles.cs
@@ -0,0 +1,63 @@
+public class Solution 
+{
+    public int FindCircleNum(int[][] M) 
+    {
+        var set = new HashSet<int>();
+        var dsu = new DisjointSetUnion(M.Length);
+
+        for (var i = 0; i < M.Length; ++i)
+            for (var j = 0; j < M[i].Length; ++j)
+                if (M[i][j] == 1)
+                    dsu.Union(i, j);
+
+        for (var i = 0; i < M.Length; ++i)
+            set.Add(dsu.Find(i));
+
+        return set.Count;
+    }
+    
+    public sealed class DisjointSetUnion
+    {
+        private readonly int[] _parent;
+        private readonly int[] _rank;
+
+        public DisjointSetUnion(int size)
+        {
+            _parent = new int[size];
+            _rank = new int[size];
+
+            for (var i = 0; i < _parent.Length; ++i)
+                _parent[i] = i;
+        }
+
+        public int Find(int x)
+        {
+            if (_parent[x] != x) _parent[x] = Find(_parent[x]);
+            return _parent[x];
+        }
+
+        public bool Union(int x, int y)
+        {
+            var xr = Find(x);
+            var yr = Find(y);
+
+            if (xr == yr) return false;
+
+            if (_rank[xr] < _rank[yr]) 
+            {
+                _parent[xr] = yr;
+            } 
+            else if (_rank[xr] > _rank[yr]) 
+            {
+                _parent[yr] = xr;
+            } 
+            else 
+            {
+                _parent[yr] = xr;
+                _rank[xr]++;
+            }
+
+            return true;
+        }
+    }
+}

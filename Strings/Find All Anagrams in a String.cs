9cf79b3086981bcf30145834fd32715c83863375
Alexander Lifshits <alexanderl1fsh1ts@gmail.com>
2020-05-18 23:32:04 +0700

    Improve task.

--- Strings/Find All Anagrams in a String.cs
+++ Strings/Find All Anagrams in a String.cs
@@ -1,7 +1,5 @@
 public class Solution 
-{    
-    private static readonly int[] mapTmp = new int[26];
-    
+{        
     public IList<int> FindAnagrams(string s, string p) 
     {
         var map = new int[26];
@@ -16,43 +14,29 @@ public class Solution
             map[c - 'a'] += 1;
             sum += c - 'a';
         }
-        
-        var contains = true;
 
-        for (var i = 0; i < p.Length; ++i)
+        var start = 0;
+
+        for (var i = 0; i < s.Length; ++i)
         {
-            sumTmp += s[i] - 'a';
             if (map[s[i] - 'a'] == 0)
-                contains = false;
-        }
+            {
+                sumTmp = 0;
+                start = i + 1;
+                continue;
+            }
 
-        if (contains && sum == sumTmp && Check(s, p, 0, map))
-            res.Add(0);
-
-        for (var i = p.Length; i < s.Length; ++i)
-        {
-            sumTmp -= s[i - p.Length] - 'a';
             sumTmp += s[i] - 'a';
+            if (i - start + 1 > p.Length)
+            {
+                sumTmp -= s[i - p.Length] - 'a';
+                ++start;
+            }
 
-            if (map[s[i] - 'a'] > 0 && sum == sumTmp && Check(s, p, i - p.Length + 1, map))
-                res.Add(i - p.Length + 1);
+            if (sum == sumTmp && i - start + 1 == p.Length)
+                res.Add(start);
         }
 
         return res;
-    }
-
-    private static bool Check(string s, string p, int start, int[] map)
-    {
-        Array.Copy(map, mapTmp, mapTmp.Length);
-
-        for (var i = start; i < start + p.Length; ++i)
-        {
-            if (mapTmp[s[i] - 'a'] == 0)
-                return false;
-
-            mapTmp[s[i] - 'a'] -= 1;
-        }
-
-        return mapTmp.All(x => x == 0);
     }
 }    
